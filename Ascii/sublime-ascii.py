801374c502cdfcd707e79f1110fb07c8fecce026
Muyr <muyanru345@163.com>
2013-12-09 14:18:18 +0800

    添加sublime-ascii.py和asciiDict.mu

---
+++ Ascii/sublime-ascii.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+###################################################################
+# Author: Mu yanru
+# Date  : 2013.12
+# Email : muyanru345@163.com
+###################################################################
+
+import sublime, sublime_plugin
+import pickle, os
+
+class AsciiCommand(sublime_plugin.TextCommand):  
+    def run(self, args, w, f='#', e=' '): 
+        print 'AsciiCommand'
+        view = self.view 
+        edit = view.begin_edit()
+        view.insert(edit, view.sel()[0].begin(), str2Ascii(w,f,e))
+        view.end_edit(edit)
+        # sublime.message_dialog("generate is success!")
+
+
+class AsciishapCommand(sublime_plugin.TextCommand):  
+    def run(self, args):
+        view = self.view
+        selRegion = view.sel()[0]
+        wordRegion = selRegion
+        if selRegion.empty():
+            wordRegion = view.word(selRegion)
+        
+        words = view.substr(wordRegion)
+        edit = view.begin_edit()
+        view.replace(edit, wordRegion, str2Ascii(words, '#'))
+        view.end_edit(edit)
+
+
+class AsciislashCommand(sublime_plugin.TextCommand):  
+    def run(self, args):
+        view = self.view
+        selRegion = view.sel()[0]
+        wordRegion = selRegion
+        if selRegion.empty():
+            wordRegion = view.word(selRegion)
+        
+        words = view.substr(wordRegion)
+        edit = view.begin_edit()
+        view.replace(edit, wordRegion, str2Ascii(words, '/'))
+        view.end_edit(edit)
+
+def str2Ascii(inputWord, fillChar = '#', emptyChar = ' '):
+    if len(fillChar) != 1: fillChar = '#'
+    if len(emptyChar) != 1: emptyChar = ' '
+    pickleFile = open(os.path.expanduser(sublime.packages_path() + '\\Ascii\\asciiDict.mu'), 'rb')
+    asciiDict = pickle.load(pickleFile)
+    pickleFile.close()    
+    outputWord = ''
+    outputList = []
+    for char in inputWord:
+        if asciiDict.has_key(char.lower()):
+            outputList.append(asciiDict.get(char.lower()))
+    contentList = []
+    for i in range(len(outputList[0])):
+        tmpList = ''
+        for j in range(len(outputList)):
+            tmpList += outputList[j][i]
+        contentList.append(tmpList)
+    for i in contentList:
+        for j in i:
+            if j == '0':
+                outputWord += emptyChar
+            elif j == '1':
+                outputWord += fillChar
+        outputWord += '\n'
+    return outputWord
